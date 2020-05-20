249ef3d067a772774b7d32da2e060b7abe0ad40d
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-05-20 15:06:23 -0700

    Registration work in progress

--- src/main/java/com/group2/bank/controllers/RegistrationController.java
+++ src/main/java/com/group2/bank/controllers/RegistrationController.java
@@ -1,4 +1,16 @@
 package com.group2.bank.controllers;
 
+import com.group2.bank.resources.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+
+    @PostMapping
+    public Response response(){
+
+        return null;
+    }
+
 }
--- src/main/java/com/group2/bank/services/RegistrationService.java
+++ src/main/java/com/group2/bank/services/RegistrationService.java
@@ -1,4 +1,30 @@
 package com.group2.bank.services;
 
+import com.group2.bank.models.User;
+import com.group2.bank.repositories.UserRepository;
+import com.group2.bank.resources.Response;
+
 public class RegistrationService {
+
+    UserRepository userRepository;
+    Response response;
+
+    public  Response createUserAccount(String firstName, String lastName, String userName, String passWord, Double balance){
+
+        //this method will do all server side validations
+
+        //validating a userName, do not allow to create if the username is already taken
+
+        User user = userRepository.findByUserName(userName);
+
+        if(user != null){
+            //Username occupied
+            return new Response(409,"user name already taken");
+        }
+
+
+
+        //TODO
+        return null;
+    }
 }
