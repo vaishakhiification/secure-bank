e2cbf1beae71c0e11804eb94740f0934cbc6c924
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-05-25 23:29:16 -0700

    Validations and checks added

--- src/main/java/com/group2/bank/services/LoginService.java
+++ src/main/java/com/group2/bank/services/LoginService.java
@@ -6,6 +6,8 @@ import com.group2.bank.resources.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 public class LoginService {
 
@@ -13,6 +15,15 @@ public class LoginService {
     UserRepository userRepository;
 
     public Response login(String userName, String password){
+
+        //Check and validate username and password
+        String regex = "[_\\-\\.0-9a-z]+";
+        if(!Pattern.matches(regex,userName)){
+            return new Response(409,"invalid username entered");
+        }
+        if(!Pattern.matches(regex,password)){
+            return new Response(409,"invalid password entered");
+        }
 
         User user = userRepository.findByUserName(userName);
 
@@ -22,7 +33,8 @@ public class LoginService {
         else if(!user.getPassword().equals(password)){
             return new Response(409,"Password is incorrect");
         }
-        //TODO think of more validations
+
+
 
         String userAsResponse = user.toString();
         return new Response(200,userAsResponse);
