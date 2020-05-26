8c1878696cf667cac216a26b73cd61ca92ce8aec
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-05-26 15:11:49 -0700

    code refactored

--- src/main/java/com/group2/bank/models/User.java
+++ src/main/java/com/group2/bank/models/User.java
@@ -1,10 +1,12 @@
 package com.group2.bank.models;
 
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class User {
