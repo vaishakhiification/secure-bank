dd0894673f3c0eefbacb8cf07fa0121d8cd588f1
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-05-26 23:49:21 -0700

    adding support for cross origin requests

--- src/main/java/com/group2/bank/controllers/TransactionController.java
+++ src/main/java/com/group2/bank/controllers/TransactionController.java
@@ -4,11 +4,13 @@ import com.group2.bank.models.User;
 import com.group2.bank.resources.Response;
 import com.group2.bank.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TransactionController {
 
     @Autowired
