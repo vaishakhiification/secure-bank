fd60304f65a06cfda4092f42aac0f267e82a2bbf
Vaishakhi Pilankar <vaishakhiification@users.noreply.github.com>
2020-06-11 14:59:54 -0700

    Update login.component.ts

--- src/main/web/angularclient/src/app/components/login/login.component.ts
+++ src/main/web/angularclient/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import {UserService} from "../../services/user/user.service";
 import {User} from "../../models/user/user";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {CryptoService} from "../../services/crypto/crypto.service";
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,21 @@ import {AuthenticationService} from "../../services/authentication/authenticatio
 })
 export class LoginComponent implements OnInit {
   user: User;
-
+  count: number;
+  readonly limit: number = 3;
+  
   constructor(private userService: UserService, private router: Router,
-              private authenticationService: AuthenticationService) {
+              private authenticationService: AuthenticationService,
+              private cryptoService: CryptoService) {
   }
 
   ngOnInit(): void {
     this.user = new User();
+    this.count = 0;
   }
 
   login() {
+    this.user.password = this.cryptoService.encrypt(this.user.password);
     this.userService.login(this.user).subscribe(
       result => {
         if (result && result.statusCode === 200) {
